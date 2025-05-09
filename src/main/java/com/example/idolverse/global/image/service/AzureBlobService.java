a797d7e9268e0a0f4f033dfbbc9c433b8e02adce
yeoeol <gnsl3528@naver.com>
2025-05-09 23:29:00 +0900

    feat: 이미지 확장자, 파일사이즈 검사 구현

--- src/main/java/com/example/idolverse/global/image/service/AzureBlobService.java
+++ src/main/java/com/example/idolverse/global/image/service/AzureBlobService.java
@@ -1,5 +1,7 @@
 package com.example.idolverse.global.image.service;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
@@ -18,11 +20,17 @@ public class AzureBlobService {
 
 	private final BlobServiceClient blobServiceClient;
 
+	private static final List<String> ALLOWED_EXTENSIONS = Arrays.asList("jpg", "jpeg", "png", "gif");
+	private static final long MAX_SIZE = 5 * 1024 * 1024; // 5MB
+
 	public String uploadFile(MultipartFile file, String containerName) {
 		BlobContainerClient containerClient = blobServiceClient.getBlobContainerClient(containerName);
 		if (!containerClient.exists()) {
 			containerClient.create();
 		}
+
+		validateExtension(file);
+		validateFileSize(file);
 
 		String uniqueFileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
 		BlobClient blobClient = containerClient.getBlobClient(uniqueFileName);
@@ -36,5 +44,19 @@ public class AzureBlobService {
 		}
 
 		return blobClient.getBlobUrl(); // 업로드된 파일 URL 반환
+	}
+
+	private void validateExtension(MultipartFile file) {
+		String originalFilename = file.getOriginalFilename();
+		String extension = originalFilename.substring(originalFilename.lastIndexOf(".") + 1).toLowerCase();
+		if (!ALLOWED_EXTENSIONS.contains(extension)) {
+			throw new RuntimeException("허용되지 않는 확장자입니다.");
+		}
+	}
+
+	private void validateFileSize(MultipartFile file) {
+		if (file.getSize() > MAX_SIZE) {
+			throw new RuntimeException("파일 크기는 5MB를 초과할 수 없습니다.");
+		}
 	}
 }
