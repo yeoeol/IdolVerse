6797317a25b74af391d0f4213f79a1db803b8e91
yeoeol <gnsl3528@naver.com>
2025-07-30 22:54:26 +0900

    feat: 게시글 응원 API 스웨거 어노테이션 추가

--- src/main/java/com/example/idolverse/domain/cheer/controller/CheerController.java
+++ src/main/java/com/example/idolverse/domain/cheer/controller/CheerController.java
@@ -3,6 +3,7 @@ package com.example.idolverse.domain.cheer.controller;
 import com.example.idolverse.domain.cheer.dto.CheerRequestDto;
 import com.example.idolverse.domain.cheer.service.CheerService;
 import com.example.idolverse.global.response.ApiResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -15,12 +16,14 @@ public class CheerController {
 
     private final CheerService cheerService;
 
+    @Operation(summary = "특정 게시글 응원")
     @PostMapping
     public ApiResponseDto<Void> cheer(@RequestBody CheerRequestDto cheerRequestDto) {
         cheerService.cheer(cheerRequestDto);
         return ApiResponseDto.success();
     }
 
+    @Operation(summary = "특정 게시글 응원 취소")
     @DeleteMapping
     public ApiResponseDto<Void> cancelCheer(@RequestBody CheerRequestDto cheerRequestDto) {
         cheerService.cancelCheer(cheerRequestDto);
